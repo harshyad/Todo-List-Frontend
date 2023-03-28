22ad2f9a3ecf634354f660b89761e84979a5ebd2
Harsh Yadav <harshyadav78200@gmail.com>
2023-03-28 11:41:01 +0530

    Frontend Done

---
+++ edit-task.js
@@ -0,0 +1,63 @@
+const taskIDDOM = document.querySelector('.task-edit-id')
+const taskNameDOM = document.querySelector('.task-edit-name')
+const taskCompletedDOM = document.querySelector('.task-edit-completed')
+const editFormDOM = document.querySelector('.single-task-form')
+const editBtnDOM = document.querySelector('.task-edit-btn')
+const formAlertDOM = document.querySelector('.form-alert')
+const params = window.location.search
+const id = new URLSearchParams(params).get('id')
+let tempName
+
+const showTask = async () => {
+  try {
+    const response = await axios.get(`http://localhost:4000/api/v1/tasks?id=${id}`)
+    const {completed,_id, name } = response.data
+    taskIDDOM.textContent = _id
+    taskNameDOM.value = name
+    tempName = name
+    if (completed) {
+      taskCompletedDOM.checked = true
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+showTask()
+
+editFormDOM.addEventListener('submit', async (e) => {
+  editBtnDOM.textContent = 'Loading...'
+  e.preventDefault()
+  try {
+    const taskName = taskNameDOM.value
+    const taskCompleted = taskCompletedDOM.checked
+
+    const response = await axios.patch(`http://localhost:4000/api/v1/tasks`, {
+      name: taskName,
+      completed: taskCompleted,
+      id
+    })
+    console.log(response.data.task)
+    const { _id, completed, name } = response.data.task
+    console.log(name)
+    taskIDDOM.textContent = _id
+    taskNameDOM.value = name
+    tempName = name
+    if (completed) {
+      taskCompletedDOM.checked = true
+    }
+    formAlertDOM.style.display = 'block'
+    formAlertDOM.textContent = `success, edited task`
+    formAlertDOM.classList.add('text-success')
+  } catch (error) {
+    console.error(error)
+    taskNameDOM.value = tempName
+    formAlertDOM.style.display = 'block'
+    formAlertDOM.innerHTML = `error, please try again`
+  }
+  editBtnDOM.textContent = 'Edit'
+  setTimeout(() => {
+    formAlertDOM.style.display = 'none'
+    formAlertDOM.classList.remove('text-success')
+  }, 3000)
+})
